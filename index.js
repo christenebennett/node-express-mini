dd8b2494c43a552edb0a305d3934f7977e742698
Christene Bennett <christene@Christenes-MacBook-Pro.local>
2019-02-27 15:22:35 -0700

    Install express to project

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // implement your API here
+const express = require('express');
+
+const server = express();
+const PORT = 5000;
+
+server.get('/', (req, res) => {
+  res.send('Hello there!')
+})
+
+server.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+})
+
