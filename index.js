5f5a3bbf59f0c2ed33528d3c5cffa6932a3cebc4
Christene Bennett <christene@Christenes-MacBook-Pro.local>
2019-02-27 19:12:19 -0700

    Add GET request to /api/users endpoint

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 // implement your API here
 const express = require('express');
 
+const db = require('./data/db.js');
+
 const server = express();
 const PORT = 5000;
+
+console.log('db', db);
 
 server.get('/', (req, res) => {
   res.send('Hello there!')
 })
+
+
+
+server.get('/api/users', (req, res) => {
+  db.find()
+    .then(users => {
+      res.json(users);
+    })
+    .catch(error => {
+      res.status(500).json({error: "The users information could not be retrieved."})
+    }
+    )
+})
+
+
+
+
+
 
 server.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
